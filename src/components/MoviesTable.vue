ed7c3999acf8d1f232e18bc617e693eea8e1a2d9
By698 <dbyszewski@gmail.com>
2019-11-18 17:58:36 +0100

    [Add] movies data into table

--- src/components/MoviesTable.vue
+++ src/components/MoviesTable.vue
@@ -9,15 +9,30 @@
           <th>Genres</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        <tr v-for="(movie, index) in movies" v-bind:key="index">
+          <td>{{movie.title}}</td>
+          <td>{{movie.year}}</td>
+          <td>{{movie.cast.join(", ")}}</td>
+          <td>{{movie.genres.join(", ")}}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
 
 <script>
+import movies from "../assets/movies.json";
+// let movie = movies[0].title
 export default {
-  name: "MoviesTable"
+  name: "MoviesTable",
+  data() {
+    return {
+      movies: movies
+    }
+  }
 };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
