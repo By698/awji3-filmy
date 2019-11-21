5ef06daef789c1f04bb5fdff8872511d36d40808
KApacze <kaczeapacze@gmail.com>
2019-11-21 23:29:31 +0100

    Select button with all actors

--- src/components/MoviesByCast.vue
+++ src/components/MoviesByCast.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="movies-by-cast">
     <h1>Movies by cast</h1>
+    <select v-model="selected"  :searchable="true"  :clearable="true" :filterable="true"  :close-on-select="true"
+    >
+      <option v-for="(artist) in UniqueCast" v-bind:key="artist">{{artist}}</option>
+    </select>
   </div>
 </template>
 
 <script>
-export default {
-  name: "MoviesByCast"
+  import json from '../assets/movies.json';
+ import {_} from 'vue-underscore'
+
+  export default {
+  name: "MoviesByCast",
+  data: () => ({
+   movies: json.valueOf(), // initialize empty array
+    // cast:
+ }),
+  computed: {
+    UniqueCast() {
+     let actors = _.map(this.movies, function(movie){
+       return movie.cast;
+     });
+     actors = _.flatten(actors);
+      let cast= _.unique(actors);
+      return cast;
+    }
+
+  }
 };
 </script>
 
