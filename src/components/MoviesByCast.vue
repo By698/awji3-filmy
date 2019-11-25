514baca8830b68ba3d364b594a7f7828a0e0ce09
By698 <dbyszewski@gmail.com>
2019-11-25 21:39:10 +0100

    Add displaying movies after selecting actor

--- src/components/MoviesByCast.vue
+++ src/components/MoviesByCast.vue
@@ -1,33 +1,57 @@
 <template>
   <div class="movies-by-cast">
     <h1>Movies by cast</h1>
-    <select v-model="selected"  :searchable="true"  :clearable="true" :filterable="true"  :close-on-select="true"
+    <select
+      class="col-sm-4"
+      v-model="selected"
+      :searchable="true"
+      :clearable="true"
+      :filterable="true"
+      :close-on-select="true"
     >
-      <option v-for="(artist) in UniqueCast" v-bind:key="artist">{{artist}}</option>
+      <option :value="null" disabled hidden>Select actor</option>
+      <option v-for="(artist) in UniqueCast" :key="artist">{{artist}}</option>
     </select>
+    <ol>
+      <li v-for="(movie, index) in SelectedActorMovies" :key="index">{{movie.title}}</li>
+    </ol>
   </div>
 </template>
 
 <script>
-  import json from '../assets/movies.json';
- import {_} from 'vue-underscore'
+import json from "../assets/movies.json";
+import { _ } from "vue-underscore";
 
-  export default {
+export default {
   name: "MoviesByCast",
   data: () => ({
-   movies: json.valueOf(), // initialize empty array
-    // cast:
- }),
+    movies: json.valueOf(),
+    selected: null
+  }),
   computed: {
     UniqueCast() {
-     let actors = _.map(this.movies, function(movie){
-       return movie.cast;
-     });
-     actors = _.flatten(actors);
-      let cast= _.unique(actors);
-      return cast;
-    }
+      /*eslint no-console: ["error", { allow: ["log", "error"] }] */
+      // console.log(this.movies)
+      let actors = _.map(this.movies, function(movie) {
+        return movie.cast;
+      });
+      actors = _.flatten(actors);
 
+      let cast = _.unique(actors);
+      return cast;
+    },
+    SelectedActorMovies() {
+      
+      if(this.selected != null){
+        let sa = this.selected;
+        let selectedActorMovies = _.filter(this.movies, function(movie){
+          return movie.cast.join().includes(sa);
+        })
+        return selectedActorMovies;
+      } else {
+        return [];
+      }
+    }
   }
 };
 </script>
